b4935a1c97db6e70ad40e8407f9be6a726aac477
nrigotti <nicolo.rigotti@gmail.com>
2022-04-22 17:59:33 +0200

    Improve Block Player Collision

--- SmashBros2D/Assets/Scripts/Collisions/BlockPlayerCollision.cs
+++ SmashBros2D/Assets/Scripts/Collisions/BlockPlayerCollision.cs
@@ -2,14 +2,24 @@
 // using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockPlayerCollision : MonoBehaviour
+namespace smash_bros
 {
-    public Collider2D characterCollider;
-    public Collider2D characterBlockerCollider;
-
-    // Update is called once per frame
-    void Update()
+    public class BlockPlayerCollision : MonoBehaviour
     {
-        Physics2D.IgnoreCollision(characterCollider, characterBlockerCollider, true);
+        private Collider2D[] characterColliders;
+        private Collider2D   characterBlockerCollider;
+
+        void Awake()
+        {
+            characterBlockerCollider = GetComponent<Collider2D>();
+            characterColliders       = transform.parent.GetComponents<Collider2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            foreach (Collider2D characterCollider in characterColliders)
+                Physics2D.IgnoreCollision(characterCollider, characterBlockerCollider, true);
+        }
     }
 }
